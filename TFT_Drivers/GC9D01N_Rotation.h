401403016d4f17ca28589aee19f3f737933aea26
Christoph Ketzler <christoph@ketzler.de>
2025-04-07 19:27:13 +0200

    Update GC9D01N_Rotation.h

--- TFT_Drivers/GC9D01N_Rotation.h
+++ TFT_Drivers/GC9D01N_Rotation.h
@@ -6,22 +6,22 @@
   writecommand(TFT_MADCTL);
   switch (rotation) {
     case 0: // Portrait
-      writedata(TFT_MAD_BGR);
+      writedata(0x00);
       _width  = _init_width;
       _height = _init_height;
       break;
     case 1: // Landscape (Portrait + 90)
-      writedata(TFT_MAD_MX | TFT_MAD_MV | TFT_MAD_BGR);
+      writedata(0x00 | TFT_MAD_MX | TFT_MAD_MV);
       _width  = _init_height;
       _height = _init_width;
       break;
     case 2: // Inverter portrait
-      writedata(TFT_MAD_MX | TFT_MAD_MY | TFT_MAD_BGR);
+      writedata(0x00 | TFT_MAD_MX | TFT_MAD_MY);
       _width  = _init_width;
       _height = _init_height;
       break;
     case 3: // Inverted landscape
-      writedata(TFT_MAD_MV | TFT_MAD_MY | TFT_MAD_BGR);
+      writedata(0x00 | TFT_MAD_MV | TFT_MAD_MY);
       _width  = _init_height;
       _height = _init_width;
       break;
